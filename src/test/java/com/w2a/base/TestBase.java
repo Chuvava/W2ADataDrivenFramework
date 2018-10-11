6b926650575145e7cbccfd5f89f4796be2de0e4d
Aleksandr Efimov <aleksandr.efimov94@mail.ru>
2018-10-11 14:32:08 +0300

    assertions and dataProvider added, AddCustomerTest created

--- src/test/java/com/w2a/base/TestBase.java
+++ src/test/java/com/w2a/base/TestBase.java
@@ -1,5 +1,7 @@
 package com.w2a.base;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,12 +9,17 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import com.w2a.utilities.ExcelReader;
 
 public class TestBase {
 	
@@ -21,7 +28,10 @@ public class TestBase {
 	public static Properties OR = new Properties();
 	public static FileInputStream fis;
 	public static Logger log = Logger.getLogger("devpinoyLogger");
+	public static ExcelReader excel = new ExcelReader(System.getProperty("user.dir") + "\\src\\test\\resources\\excel\\testdata.xlsx");
+	public static WebDriverWait wait;
 	
+
 	@BeforeSuite
 	public void setUp() throws FileNotFoundException {
 		if(driver == null) {
@@ -59,9 +69,21 @@ public class TestBase {
 			log.debug("navigated to: " + config.getProperty("testsiteurl"));
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicitWait")), TimeUnit.SECONDS);
+			wait = new WebDriverWait(driver, 5);
 		}
 		
 		
+	}
+	
+	public boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+			
+		} catch(NoSuchElementException e) {
+			
+			return false;
+		}
 	}
 	
 	@AfterSuite
--- src/test/java/com/w2a/testcases/BankManagerLoginTest.java
+++ src/test/java/com/w2a/testcases/BankManagerLoginTest.java
@@ -1,17 +1,21 @@
 package com.w2a.testcases;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.w2a.base.TestBase;
 
-public class LoginTest extends TestBase {
+public class BankManagerLoginTest extends TestBase {
 	
 	@Test
 	public void LoginAsBankManager() throws InterruptedException {
 		log.debug("Inside LoginTest");
 		driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();;
 		Thread.sleep(3500);
+		
+		Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("addCustBtn"))),
+				"Element: " + OR.getProperty("addCustBtn") + " is not displayed on the page");
 		
 		log.debug("Successfully executed");
 	}
